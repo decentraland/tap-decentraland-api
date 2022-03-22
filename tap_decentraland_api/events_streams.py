260976f16e1c7fc5fdf1c0b57d5c9781d636d579
Rodrigo Antonioli <rodrigo@decentraland.org>
2022-03-22 19:22:06 -0300

    initial commit

---
+++ tap_decentraland_api/events_streams.py
@@ -0,0 +1,71 @@
+"""Stream class for tap-decentraland-api."""
+
+
+import requests
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+from singer_sdk.streams import RESTStream
+
+
+
+from singer_sdk.authenticators import (
+    APIAuthenticatorBase,
+    SimpleAuthenticator,
+    OAuthAuthenticator,
+    OAuthJWTAuthenticator
+)
+
+from singer_sdk.typing import (
+    ArrayType,
+    BooleanType,
+    DateTimeType,
+    IntegerType,
+    NumberType,
+    ObjectType,
+    PropertiesList,
+    Property,
+    StringType,
+)
+
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+
+class DecentralandAPIStream(RESTStream):
+    """DecentralandAPI stream class."""
+
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        return self.config["api_url"]
+
+
+class EventsStream(DecentralandAPIStream):
+    name = "events"
+
+    path = "/api/events"
+
+    primary_keys = ['id']
+    replication_key = None
+    
+    schema = PropertiesList(
+        Property("id", StringType, required=True),
+        Property("name", StringType),
+        Property("type", StringType),
+        Property("x", IntegerType),
+        Property("y", IntegerType),
+        Property("updatedAt", IntegerType),
+        Property("top", BooleanType),
+        Property("left", BooleanType),
+        Property("topLeft", BooleanType),
+        Property("estateId", StringType),
+        Property("owner", StringType),
+        Property("tokenId", StringType),
+        Property("price", NumberType),
+    ).to_dict()
+
+
+    def parse_response(self, response) -> Iterable[dict]:
+        """Parse Tiles rows"""
+
+        data =response.json().get("data")
+        for _, t in data.items():
+            yield t
